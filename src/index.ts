91f4aa17c3cea7b2f6886d74c6547357686332b6
ArthurLelis <arthurleliscc@gmail.com>
2025-03-04 11:59:48 -0300

    alt

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import routes from './routes';
+import { VercelRequest, VercelResponse } from '@vercel/node';
 
 const port = 8787;
 const app = express();
@@ -10,3 +11,8 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(port, () => console.log(`🔥 Server started at `));
+
+export default (
+  request: VercelRequest,
+  response: VercelResponse
+) => app(request, response);
