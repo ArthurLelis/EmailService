a402f815a1c821df8f190f4360f1a7ea30baee87
ArthurLelis <arthurleliscc@gmail.com>
2025-03-04 14:02:36 -0300

    alt

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app = express();
 
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://mzassessoriafinanceira.com.br',
+  'https://mzassessoriafinanceira.com.br/',
 ];
 
 app.use(
