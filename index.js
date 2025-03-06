1c3cc32dec9aaf2cf67bb653170a9c341ad42874
ArthurLelis <arthurleliscc@gmail.com>
2025-03-06 12:04:47 -0300

    fix

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-// import cors from 'cors';
-// import express, { Request, Response, NextFunction } from 'express';
-
-// const app = express();
-
-// app.use(cors({
-//   origin: 'https://mzassessoriafinanceira.com.br',
-//   methods: 'GET, POST, OPTIONS',
-//   allowedHeaders: 'Content-Type, Authorization',
-//   credentials: true,
-// }));
-
-// app.use((req, res, next) => {
-//   res.setHeader('Content-Type', 'application/json');
-//   next();
-// });
-
-// app.use(express.json());
-
-// export default app;
-
 import cors from 'cors';
 import express from 'express';
 
@@ -27,15 +6,8 @@ import router from './src/routes';
 const port = 3005;
 const app = express()
 
+app.use(express.json());
 app.use(cors());
 app.use(router);
-
-// app.get('/', (req, res) => {
-//   res.send('Express Typescript on Vercel')
-// });
-
-// app.get('/ping', (req, res) => {
-//   res.send('pong 🏓')
-// });
 
 app.listen(port, () => console.log(`Server is listening on ${port}`));
