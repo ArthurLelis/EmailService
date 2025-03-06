c82530dafa7e6c8af8f2e2ff4d46e08c5a8c1ff3
ArthurLelis <arthurleliscc@gmail.com>
2025-03-06 10:57:40 -0300

    fix dev.to

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,40 @@
-import cors from 'cors';
-import express, { Request, Response, NextFunction } from 'express';
+// import cors from 'cors';
+// import express, { Request, Response, NextFunction } from 'express';
 
-import router from './routes';
+// import router from './routes';
 
-const app = express();
+// const app = express();
 
-app.use(cors({
-  origin: 'https://mzassessoriafinanceira.com.br',
-  methods: 'GET, POST, OPTIONS',
-  allowedHeaders: 'Content-Type, Authorization',
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: 'https://mzassessoriafinanceira.com.br',
+//   methods: 'GET, POST, OPTIONS',
+//   allowedHeaders: 'Content-Type, Authorization',
+//   credentials: true,
+// }));
 
-app.use((req, res, next) => {
-  res.setHeader('Content-Type', 'application/json');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Content-Type', 'application/json');
+//   next();
+// });
 
-app.use(express.json());
-app.use(router);
+// app.use(express.json());
+// app.use(router);
 
-export default app;
+// export default app;
+
+import express, { Request, Response } from 'express';
+
+const app = express()
+const port = process.env.PORT || 8080
+
+app.get('/', (_req: Request, res: Response) => {
+  return res.send('Express Typescript on Vercel')
+})
+
+app.get('/ping', (_req: Request, res: Response) => {
+  return res.send('pong 🏓')
+})
+
+app.listen(port, () => {
+  return console.log(`Server is listening on ${port}`)
+})
